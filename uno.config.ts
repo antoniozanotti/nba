264ce7279df47bdba22979ded08436135a07ed13
Antonio Zanotti <antoniorzanotti@gmail.com>
2025-01-14 15:15:31 -0300

    build: set up dependencies (Vite + Typescript + Tailwind + Jest + Testing Library + Axios)

---
+++ uno.config.ts
@@ -0,0 +1,5 @@
+import { presetWind, defineConfig } from "unocss";
+
+export default defineConfig({
+  presets: [presetWind()],
+});
---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import UnoCSS from "unocss/vite";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [vue(), UnoCSS()],
+});
